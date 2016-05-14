4b83b00062e027105c3ea74ba25418b29554c6b9
dhaval nagar <dhaval.b.nagar@gmail.com>
2016-05-14 12:05:26 +0530

    frequent update logic with timer

--- BGLocation/ViewController.swift
+++ BGLocation/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
     let locationManager = CLLocationManager()
     var lastLocation: CLLocation?;
     var debug: Bool = true;
+    var timer: NSTimer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,13 +32,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         self.locationManager.delegate = self
         
         // Accuracty is set to medium and will be adjusted later
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
         
         self.locationManager.requestAlwaysAuthorization();
         
         // Significant Changes will be used later on for passive updates
         self.locationManager.startMonitoringSignificantLocationChanges();
-        
+
         // Notifications for debuging purpose
         if debug {
             let notificationSettings = UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: nil)
@@ -49,12 +50,42 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         self.locationManager.stopMonitoringSignificantLocationChanges()
     }
     
+    func startFrequentLocationUpdates(){
+        //self.locationManager.startUpdatingLocation()
+    }
+    
+    func stopFrequentLocationUpdates(){
+        //self.locationManager.stopUpdatingLocation()
+    }
+    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         // Distance Filter Login Here
+        self.startFrequentLocationUpdates()
         
         if debug {
             showNotification(manager.location!.coordinate);
         }
+    }
+    
+    // Receive Frequent Updates, reset timer on every udpate
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        print("Frequent Update: \(newLocation.coordinate.latitude), \(newLocation.coordinate.longitude)")
+        
+        if let t = timer{
+            t.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: Selector("timerFired"), userInfo: nil, repeats: true)
+        
+        if debug {
+            showNotification(newLocation.coordinate);
+        }
+    }
+    
+    // Stop Frequent Updates
+    func timerFired(){
+        print("Frequent Updates Stopped")
+        self.stopFrequentLocationUpdates()
     }
     
     func showNotification(let coordinate : CLLocationCoordinate2D){
